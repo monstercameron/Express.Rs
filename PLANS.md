b36961bd34b800ebd8de2139b478562637c648a3
monstercameron <mr.e.cameron@gmail.com>
2019-09-15 23:43:42 -0400

    planning init

---
+++ PLANS.md
@@ -0,0 +1,9 @@
+learn how to accept incoming tcp requests
+learn how to send outbound tcp responses
+
+figure out tokio and the event loop
+
+
+start thinking about middleware pipeline
+
+start researching threading and scalability
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+// use std::io::prelude::*;
+// use std::net::TcpStream;
+// use std::net::TcpListener;
+
+extern crate tokio;
+
+use tokio::prelude::*;
+use tokio::io::copy;
+use tokio::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Server!");
+    // run();
+    server();
+}
+
+// fn run() {
+//     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+//     for stream in listener.incoming() {
+//         let stream = stream.unwrap();
+
+//         handle_connection(stream);
+//     }
+// }
+
+// fn handle_connection(mut stream: TcpStream) {
+//     let mut buffer = [0; 512];
+
+//     stream.read(&mut buffer).unwrap();
+
+//     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+// }
+
+fn server() {
+    // Bind the server's socket.
+    let addr = "127.0.0.1:12345".parse().unwrap();
+    let listener = TcpListener::bind(&addr)
+        .expect("unable to bind TCP listener");
+
+    // Pull out a stream of sockets for incoming connections
+    let server = listener.incoming()
+        .map_err(|e| eprintln!("accept failed = {:?}", e))
+        .for_each(|sock| {
+            // Split up the reading and writing parts of the
+            // socket.
+            let (reader, writer) = sock.split();
+
+            // A future that echos the data and returns how
+            // many bytes were copied...
+            let bytes_copied = copy(reader, writer);
+
+            // ... after which we'll print what happened.
+            let handle_conn = bytes_copied.map(|amt| {
+                println!("wrote {:?} bytes", amt)
+            }).map_err(|err| {
+                eprintln!("IO error {:?}", err)
+            });
+
+            // Spawn the future as a concurrent task.
+            tokio::spawn(handle_conn)
+        });
+
+    // Start the Tokio runtime
+    tokio::run(server);
 }
